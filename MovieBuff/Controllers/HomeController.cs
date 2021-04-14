e0fa89f6d18178b9fa371b3220f176652525f730
akhilskumar <sk.akhil4u@gmail.com>
2021-04-14 08:16:23 +0530

    Add project files.

---
+++ MovieBuff/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieBuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Movie()
+        {
+
+            return View();
+        }
+    }
+}
