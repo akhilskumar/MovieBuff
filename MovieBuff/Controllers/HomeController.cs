a4eb2dd4b865d5fc9cb5a26c2c468315b0731352
akhilskumar <sk.akhil4u@gmail.com>
2021-04-14 09:10:46 +0530

    Action Parametres

--- MovieBuff/Controllers/HomeController.cs
+++ MovieBuff/Controllers/HomeController.cs
@@ -17,5 +17,23 @@ namespace MovieBuff.Controllers
             
             return View(movies);
         }
+
+        public ActionResult Edit(string id)
+        {
+            return Content("id="+id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortby))
+            {
+                sortby = "Name";
+            }
+            return Content(string.Format("Page Index={0} & sortBy={1}", pageIndex, sortby));
+        }
     }
 }
